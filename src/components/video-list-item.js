23258fdd7795c65c746a7b8827d3b1aa8189163a
Meg4R0M <psykoterro@gmail.com>
2018-12-22 15:55:08 +0100

    Liste dynamique de bande annonce de films

---
+++ src/components/video-list-item.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500/";
+
+const VideoListItem = (props) => {
+  const {movie} = props;
+  return (
+      <li className="list-group-item" onClick={handleOnClick}>
+        <div className="media">
+          <div className="media-left">
+            <img src={`${IMAGE_BASE_URL}${movie.poster_path}`} alt={movie.title} height="150px" className="media-object img-rounded"/>
+          </div>
+          <div className="media-body">
+            <h5 className="title_list_item">{movie.title}</h5>
+          </div>
+        </div>
+      </li>
+  )
+
+  function handleOnClick(){
+    props.callback(movie);
+  }
+
+};
+
+export default VideoListItem;
